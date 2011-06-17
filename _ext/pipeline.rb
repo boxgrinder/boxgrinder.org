5c63ae7f58f4e577dc51105272709ebb8ff4bec6
Marek Goldmann <marek.goldmann@gmail.com>
2011-06-17 15:46:02 +0200

    fixed atom link issue

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -8,8 +8,8 @@ require 'rss_widget'
 Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog')
   extension Awestruct::Extensions::Paginator.new(:posts, '/blog/index', :per_page => 5)
-  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Atomizer.new(:posts, '/blog.atom')
   extension Awestruct::Extensions::IntenseDebate.new
   extension ReleaseSizes.new
   extension TOC.new(:levels => 3)
